0c2720624b4b140467a65d8df2d358d2d3b90763
luchi <pazluciano.dev@gmail.com>
2024-09-05 19:25:50 -0300

    change

--- server/db.js
+++ server/db.js
@@ -1,9 +1,9 @@
 import { Sequelize } from "sequelize";
-import { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } from "./src/config/envs.js";
+import { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_NAME, POSTGRES_URL } from "./src/config/envs.js";
 
 
 export const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `${POSTGRES_URL}://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}/${POSTGRES_NAME}`,
   {
     logging: false,
     native: false,
--- server/src/controllers/servicesControllers.js
+++ server/src/controllers/servicesControllers.js
@@ -1,23 +1,33 @@
 import { Category } from "../models/Category.js";
 import { Service } from "../models/Services.js";
 
-export async function createService(title, description, category) {
-  const service = await Service.findOne({ where: { title } });
-  if (service) {
-    throw new Error(`El servicio con título ${title} ya existe`);
+export async function createService(title, description, categories) {
+  try {
+    const service = await Service.findOne({ where: { title } });
+    if (service) {
+      throw new Error(`El servicio con título ${title} ya existe`);
+    }
+  
+    const newService = await Service.create({
+      title,
+      description
+    });
+    const cats = await Category.findAll({where : { name : categories }})
+    if(!cats || cats.length == 0) throw new Error("Name of categories invalid")
+    newService.addCategories(cats);
+    return newService;
+  } catch (error) {
+    throw new Error(" Error creating service")
   }
-
-  const newService = await Service.create({
-    title,
-    description,
-    category,
-  });
-
-  return newService;
 }
 
 export async function getAllServices() {
-  const services = await Service.findAll({include : [{ model :Category, as : 'category'}]});
+  const services = await Service.findAll({include: {
+    model: Category,
+    as: 'categories', 
+    attributes: ['name'],
+  },
+  attributes: ['id', 'title', 'description']});
   return services;
 }
 
@@ -60,49 +70,69 @@ export async function generateBasicServices() {
     const gardenCategory = await Category.findOne({ where: { name: 'Jardín' } });
     const automotiveCategory = await Category.findOne({ where: { name: 'Automotriz' } });
     const healthCategory = await Category.findOne({ where: { name: 'Salud y Bienestar' } });
-
     if (!homeCategory || !gardenCategory || !automotiveCategory || !healthCategory) {
       throw new Error('Una o más categorías no se encontraron.');
     }
 
-    await Service.bulkCreate([
-      { title: 'Plomería', description: 'Servicio de plomería para reparar y mantener tuberías.', categoryId: homeCategory.id },
-      { title: 'Carpintería', description: 'Servicios de carpintería para muebles y estructuras de madera.', categoryId: homeCategory.id },
-      { title: 'Electricidad', description: 'Instalación y reparación de sistemas eléctricos.', categoryId: homeCategory.id },
-      { title: 'Pintura', description: 'Pintura de interiores y exteriores.', categoryId: homeCategory.id },
-      { title: 'Limpieza', description: 'Servicios de limpieza general y especializada.', categoryId: homeCategory.id },
-      { title: 'Cerradura', description: 'Reparación e instalación de cerraduras y sistemas de seguridad.', categoryId: homeCategory.id },
-      { title: 'Fontanero', description: 'Reparación y mantenimiento de sistemas de fontanería.', categoryId: homeCategory.id },
-      { title: 'Techista', description: 'Instalación y reparación de techos.', categoryId: homeCategory.id },
-      { title: 'Gasista', description: 'Servicios de instalación y mantenimiento de sistemas de gas.', categoryId: homeCategory.id },
-      { title: 'Albañil', description: 'Construcción y reparación de estructuras de ladrillo y piedra.', categoryId: homeCategory.id },
-      { title: 'Decoración', description: 'Servicios de decoración para interiores y exteriores.', categoryId: homeCategory.id },
-      { title: 'Aberturas', description: 'Instalación y reparación de ventanas y puertas.', categoryId: homeCategory.id },
-      { title: 'Durlock', description: 'Instalación y reparación de paneles de durlock.', categoryId: homeCategory.id },
+    const homeServices = await Service.bulkCreate([
+      { title: 'Plomería', description: 'Servicio de plomería para reparar y mantener tuberías.',  },
+      { title: 'Carpintería', description: 'Servicios de carpintería para muebles y estructuras de madera.',  },
+      { title: 'Electricidad', description: 'Instalación y reparación de sistemas eléctricos.',  },
+      { title: 'Pintura', description: 'Pintura de interiores y exteriores.',  },
+      { title: 'Limpieza', description: 'Servicios de limpieza general y especializada.',  },
+      { title: 'Cerradura', description: 'Reparación e instalación de cerraduras y sistemas de seguridad.', },
+      { title: 'Fontanero', description: 'Reparación y mantenimiento de sistemas de fontanería.',  },
+      { title: 'Techista', description: 'Instalación y reparación de techos.',  },
+      { title: 'Gasista', description: 'Servicios de instalación y mantenimiento de sistemas de gas.',  },
+      { title: 'Albañil', description: 'Construcción y reparación de estructuras de ladrillo y piedra.',  },
+      { title: 'Decoración', description: 'Servicios de decoración para interiores y exteriores.',  },
+      { title: 'Aberturas', description: 'Instalación y reparación de ventanas y puertas.', },
+      { title: 'Durlock', description: 'Instalación y reparación de paneles de durlock.',  },
     ]);
+    await Promise.all(
+      homeServices.map(async(ser) => {
+        await ser.addCategory(homeCategory)
+      }
+    ))
 
-    await Service.bulkCreate([
-      { title: 'Cortador de pasto', description: 'Servicio de corte de césped y mantenimiento de jardines.', categoryId: gardenCategory.id },
-      { title: 'Poda de árboles', description: 'Poda y mantenimiento de árboles y arbustos.', categoryId: gardenCategory.id },
-      { title: 'Instalación de riegos', description: 'Instalación de sistemas de riego para jardines y áreas verdes.', categoryId: gardenCategory.id },
-      { title: 'Mantenimiento de piletas', description: 'Limpieza y mantenimiento de piscinas.', categoryId: gardenCategory.id },
+    const garderServices =await Service.bulkCreate([
+      { title: 'Cortador de pasto', description: 'Servicio de corte de césped y mantenimiento de jardines.' },
+      { title: 'Poda de árboles', description: 'Poda y mantenimiento de árboles y arbustos.' },
+      { title: 'Instalación de riegos', description: 'Instalación de sistemas de riego para jardines y áreas verdes.' },
+      { title: 'Mantenimiento de piletas', description: 'Limpieza y mantenimiento de piscinas.' },
     ]);
+    await Promise.all(
+      garderServices.map(async (ser) => {
+        await ser.addCategory(gardenCategory);
+      })
+    )
 
-    await Service.bulkCreate([
-      { title : 'Servicio completo', description: 'Servicio de mantenimiento general para vehículos.', categoryId: automotiveCategory.id },
-      { title : 'Frenos', description: 'Reparación y mantenimiento del sistema de frenos.', categoryId: automotiveCategory.id },
-      { title : 'Cambio de aceites', description: 'Cambio de aceite y filtros para vehículos.', categoryId: automotiveCategory.id },
-      { title : 'Tapicero', description: 'Reparación y mantenimiento de tapicería de vehículos.', categoryId: automotiveCategory.id },
-      { title : 'Auxilio mecánico', description: 'Asistencia mecánica en carretera.', categoryId: automotiveCategory.id },
+
+    const autoServices = await Service.bulkCreate([
+      { title : 'Servicio completo', description: 'Servicio de mantenimiento general para vehículos.' },
+      { title : 'Frenos', description: 'Reparación y mantenimiento del sistema de frenos.' },
+      { title : 'Cambio de aceites', description: 'Cambio de aceite y filtros para vehículos.' },
+      { title : 'Tapicero', description: 'Reparación y mantenimiento de tapicería de vehículos.' },
+      { title : 'Auxilio mecánico', description: 'Asistencia mecánica en carretera.' },
     ]);
+    await Promise.all(
+      autoServices.map(async (ser) => {
+        await ser.addCategory(automotiveCategory);
+      })
+    )
 
-    await Service.bulkCreate([
+    const healthServices = await Service.bulkCreate([
       { title: 'Peluquería', description: 'Cortes de cabello y servicios de peluquería.', categoryId: healthCategory.id },
       { title: 'Masajes', description: 'Masajes relajantes y terapéuticos.', categoryId: healthCategory.id },
       { title: 'Manicura', description: 'Servicios de manicura y cuidado de uñas.', categoryId: healthCategory.id },
       { title: 'Pedicura', description: 'Servicios de pedicura y cuidado de pies.', categoryId: healthCategory.id },
       { title: 'Kinesiología', description: 'Tratamientos de kinesiología y rehabilitación.', categoryId: healthCategory.id },
     ]);
+    await Promise.all(
+      healthServices.map(async (ser) => {
+        await ser.addCategory(healthCategory)
+      })
+    )
 
     return true;
   } catch (error) {
