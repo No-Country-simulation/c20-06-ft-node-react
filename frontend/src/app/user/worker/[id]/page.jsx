469e7c4b3fc0e950f7eb91ff564c0c2e993c6c54
LNahuelFB <lnahuelfernandezb@gmail.com>
2024-09-11 04:00:55 -0300

    Agregados los comentarios en las paginas de los workers

--- frontend/src/app/user/worker/[id]/page.jsx
+++ frontend/src/app/user/worker/[id]/page.jsx
@@ -1,19 +1,4 @@
-async function getWorkerData(id) {
-  try {
-    const response = await fetch(`http://localhost:3000/api/workers/${id}`, {
-      cache: 'no-store',
-    });
-
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    return response.json();
-  } catch (error) {
-    console.error('Error fetching worker data:', error);
-    return null;
-  }
-}
+import styles from './styles.module.css';
 
 export default async function WorkerPage({ params }) {
   const { id } = params;
@@ -31,7 +16,40 @@ export default async function WorkerPage({ params }) {
       <p>Ciudad: {worker.city}</p>
       <p>Servicios: {worker.services.join(', ')}</p>
       <p>{worker.description}</p>
+      <h3>Seccion de reseñas</h3>
+      <ReviewsList reviews={worker.reviews}/>
     </div>
   );
 }
 
+const ReviewsList = ({ reviews = [] }) => {
+
+  return (
+    <ul className={styles.reviewList}>
+      {reviews.map((review) => (
+        <li key={review.id} className={styles.reviewItem}>
+          <strong>{review.user}</strong>
+          <p>Puntuacion: {review.rating}</p>
+          <p>Comentario: {review.comment}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+async function getWorkerData(id) {
+  try {
+    const response = await fetch(`http://localhost:3000/api/workers/${id}`, {
+      cache: 'no-store',
+    });
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    return response.json();
+  } catch (error) {
+    console.error('Error fetching worker data:', error);
+    return null;
+  }
+}
