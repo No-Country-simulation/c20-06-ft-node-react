2e18ff1a6c455a2a9e241136bb236c58c5a30f0a
luchi <pazluciano.dev@gmail.com>
2024-09-19 18:26:56 -0300

    login errorrs

--- server/src/handlers/loginHandlers.js
+++ server/src/handlers/loginHandlers.js
@@ -6,13 +6,14 @@ export async function loginHandlers(req, res) {
     const { email, password } = req.body;
     const reslutLogin = await loginUser( email, password);
     if(reslutLogin){
-      const data = {id : reslutLogin.dataValues.id, role : reslutLogin.dataValues.role}
+      const data = {id : reslutLogin.id, role : reslutLogin.role}
       const token = jwt.sign(data, 'SECRET-WORD', {expiresIn : '24h'});
       res.cookie('token', token);
     }
-    delete reslutLogin.dataValues.password
+    delete reslutLogin.password
     res.status(200).json(reslutLogin);
   } catch (error) {
+    console.log(error);
     res.status(400).json({ ok : false, error: error.message });
   }
 }
